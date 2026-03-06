365897869a41f1edb1b6311819f2643b36b48e4b
pulipatisatish <pulipati.satish2012@gmail.com>
2026-03-06 21:06:16 +1100

    refactor: move weather data logic into WeatherApp class

--- weather.js
+++ weather.js
@@ -1,68 +1,34 @@
 
-//alert("weather.js running....")
-async function getWeatherData() {
-  const url =
-    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=relativehumidity_2m";
 
-    try {
-    const request = new Request(url);
+class WeatherApp
+{
+
+    constructor ()
+    {
+        this.url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=relativehumidity_2m";
+    }
+
+ async  getWeatherData()
+ {
+    const request = new Request(this.url);
     const response = await fetch(request);
     const data = await response.json();
+    return data;
 
+ }
+
+ getTemperature(data)
+ {
     const tempUnit = data.current_weather_units.temperature;
-    const temp = data.current_weather.temperature;
-    const tempEl = document.querySelector(".t1");
+    const tempValue = data.current_weather.temperature;
+    const tempEl = document.querySelector(".temp");
+    tempEl.textContent = tempValue + tempUnit;
 
-    const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    const months = ["January", "February","March","April","May","June","July","August","September","December"];
+ }
 
-    const timeUnit = data.current_weather_units.time;
-    const time = data.current_weather.time;
-    const timeEl = document.querySelector(".td");
 
-    const formatDate = new Date(time);
-    const day = days[formatDate.getDay()];
-    const month = months[formatDate.getMonth()];
-    const date = formatDate.getDate();
-    const year = formatDate.getFullYear();
-    const displayDate = day + "  " + "" +","+""+ date  +"  "+ month + "," + year; 
 
-    const windSpeedUnit = data.current_weather_units.windspeed;
-    const windSpeed = data.current_weather.windspeed;
-    const windSpeedEl = document.querySelector(".wind-speed");
-
-    
-    if(tempEl && timeEl && windSpeedEl )
-    {
-         tempEl.textContent = temp+tempUnit;
-         timeEl.textContent = displayDate ;
-         windSpeedEl.textContent = windSpeed + windSpeedUnit;
-    }
-    else
-    {
-        console.log("Span element t1 not found.");
-    }          
-        
-    } catch (error) {
-
-        console.error("Error", error);
-        
-    }   
-    
 }
 
-getWeatherData();
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+const app = new WeatherApp();
+app.getWeatherData();
